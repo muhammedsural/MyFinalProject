95ff1da0dc8a65c9bd156b3d23aec579b4bbdbcf
Muhammed Sural <59420199+muhammedsural@users.noreply.github.com>
2021-09-27 17:51:37 +0300

    Initial commit

---
+++ Business/Concrete/ProductManeger.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManeger : IProductService
+    {
+        IProductDal _ProductDal;
+
+        public ProductManeger(IProductDal productDal)
+        {
+            _ProductDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş Kodları yazılır.
+            //Yetkisi var mı?
+            return _ProductDal.GetAll();
+        }
+    }
+}
---
+++ ConsolUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsolUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManeger productManeger = new ProductManeger(new InMemoryProductDal());
+
+            foreach (var item in productManeger.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+    }
+}
