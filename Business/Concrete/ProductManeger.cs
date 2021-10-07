0cabd45adc63ba9c1e9673aa93c0110ed303f248
Muhammed Sural <59420199+muhammedsural@users.noreply.github.com>
2021-10-07 16:11:18 +0300

    07.10.21

--- Business/Concrete/ProductManeger.cs
+++ Business/Concrete/ProductManeger.cs
@@ -28,6 +28,8 @@ namespace Business.Concrete
             _categoryService = categoryService;
         }
 
+        //Claim
+        [SecuredOperation("admin","editor")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
